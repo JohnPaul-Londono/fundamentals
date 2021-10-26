dba18621da59bd210dbaa6057e4fe1d96ee1e4ce
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-26 14:04:04 -0500

    Chaining method

--- user/bankuser.py
+++ user/bankuser.py
@@ -5,9 +5,11 @@ class User:
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def make_withdrawawl(self, amount):
         self.balance -= amount
+        return self 
 
     def display_user_balance(self):
         return self.balance
@@ -24,29 +26,21 @@ user3 = User("Bumi", 2500)
 # print(user2.balance)
 # print(user3.balance)
 
-# print(user1.balance)
-# user1.make_deposit(400)
-# user1.make_deposit(600)
-# user1.make_deposit(800)
-# user1.make_withdrawawl(400)
-# print(user1.balance)
+print(user1.balance)
+user1.make_deposit(400).make_deposit(600).make_deposit(800).make_withdrawawl(400)
+print(user1.balance)
 
-# print(user2.balance)
-# user2.make_deposit(700)
-# user2.make_deposit(1700)
-# user2.make_withdrawawl(600)
-# print(user2.balance)
+print(user2.balance)
+user2.make_deposit(700).make_deposit(1700).make_withdrawawl(600)
+print(user2.balance)
 
+print(user3.balance)
+user3.make_deposit(4600).make_withdrawawl(800).make_withdrawawl(600).make_withdrawawl(400)
+print(user2.balance)
+
+# transfer
+# print(user2.balance)
 # print(user3.balance)
-# user3.make_deposit(4600)
-# user3.make_withdrawawl(800)
-# user3.make_withdrawawl(600)
-# user3.make_withdrawawl(400)
+# User.transfer(user2, user3,500)
 # print(user2.balance)
-
-
-print(user2.balance)
-print(user3.balance)
-User.transfer(user2, user3,500)
-print(user2.balance)
-print(user3.balance)
+# print(user3.balance)
