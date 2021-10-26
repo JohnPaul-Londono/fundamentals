889f517dc996cbb6375eeecb31f34eeca2b7fd75
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-26 13:49:04 -0500

    User challenge and transer

---
+++ user/bankuser.py
@@ -0,0 +1,52 @@
+class User:
+    def __init__(self, name, amount):
+        self.name = name
+        self.balance = amount
+
+    def make_deposit(self, amount):
+        self.balance += amount
+
+    def make_withdrawawl(self, amount):
+        self.balance -= amount
+
+    def display_user_balance(self):
+        return self.balance
+
+    def transfer(from_user, to_user, amount):
+        from_user.make_withdrawawl(amount)
+        to_user.make_deposit(amount)
+
+user1 = User("JonL", 500)
+user2 = User("Kya", 2000 )
+user3 = User("Bumi", 2500)
+
+# print(user1.balance)
+# print(user2.balance)
+# print(user3.balance)
+
+# print(user1.balance)
+# user1.make_deposit(400)
+# user1.make_deposit(600)
+# user1.make_deposit(800)
+# user1.make_withdrawawl(400)
+# print(user1.balance)
+
+# print(user2.balance)
+# user2.make_deposit(700)
+# user2.make_deposit(1700)
+# user2.make_withdrawawl(600)
+# print(user2.balance)
+
+# print(user3.balance)
+# user3.make_deposit(4600)
+# user3.make_withdrawawl(800)
+# user3.make_withdrawawl(600)
+# user3.make_withdrawawl(400)
+# print(user2.balance)
+
+
+print(user2.balance)
+print(user3.balance)
+User.transfer(user2, user3,500)
+print(user2.balance)
+print(user3.balance)
